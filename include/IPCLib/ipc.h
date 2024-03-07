63b495f317ee8737586f309fb24f17278d8cae14
elitewhale75 <realvignesh@gmail.com>
2024-03-06 16:32:41 -0800

    implemented bind syscall

--- include/IPCLib/ipc.h
+++ include/IPCLib/ipc.h
@@ -1,4 +1,7 @@
+#include <sys/un.h>
+
 namespace ipc {
     int listen();
     int send();
+    int spawnSocket(sockaddr_un * sock , char const * sockName);
 };
--- src/ipc.cpp
+++ src/ipc.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include "../include/IPCLib/ipc.h"
+#include <unistd.h>
+#include <sys/socket.h>
+
+#define BUFFER_SIZE 128
 
 int ipc::listen(){
     std::cout << "Listening to process: " << std::endl;
@@ -9,4 +13,44 @@ int ipc::listen(){
 int ipc::send(){
     std::cout << "Sending to process: " << std::endl;
     return 0;
+}
+
+/*Takes in a path a pointer to a socket. Binds socket to address
+ * And returns file descriptor */
+int ipc::spawnSocket(sockaddr_un * sock , char const * sockName){
+
+    int ret;
+    int connection_socket;
+
+    /* In case program exiteded inadvertently on last run
+     * remove socket */
+    unlink(sockName);
+    /* Create Master Socket */
+    connection_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    if(connection_socket == -1){
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    std::cout << "Socket was created successfully" << std::endl;
+
+    /*Initialize socket*/
+    memset(sock, 0, sizeof(struct sockaddr_un));
+
+    /*Specify socket credentials*/
+    sock -> sun_family = AF_UNIX;
+    strncpy(sock->sun_path, sockName, sizeof(sock->sun_path) - 1);
+
+    /*Bind syscall*/
+    ret = bind(connection_socket, 
+            (const struct sockaddr *) sock, 
+            sizeof(sockaddr_un));
+
+    if(ret == -1){
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+    std::cout << "Socket successfully binded" << std::endl;
+
+    return connection_socket;
 }
